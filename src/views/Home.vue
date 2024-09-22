2509e323e42c1d23bb464c4c69a4bc10e2ac36cc
yutthaphichai08 <yutthaphichaikk@gmail.com>
2024-09-22 11:46:43 +0700

    create form add content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,59 @@
 <script>
+import Swal from "sweetalert2";
+
 export default {
   data() {
     return {
       items: [],
+      newContent: { content: "" },
     };
   },
   methods: {
     async fetchData() {
       try {
-        const response = await fetch("http://localhost:3333/api/content");
+        const response = await fetch("http://localhost:3333/api/content", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         this.items = await response.json();
-        // console.log("Data fetched successfully:", this.items);
       } catch (error) {
         console.error("Error fetching data:", error);
+      }
+    },
+    async addItem() {
+      try {
+        const response = await fetch("http://localhost:3333/api/insert", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this.newContent),
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        // console.log("Item added:", data);
+        this.newContent = data;
+
+        Swal.fire({
+          title: "สำเร็จ!",
+          text: "เพิ่มข้อมูลเรียบร้อยแล้ว",
+          icon: "success",
+          confirmButtonText: "ตกลง",
+        });
+
+        this.fetchData();
+      } catch (error) {
+        console.log("Error adding content:", error);
       }
     },
   },
@@ -27,8 +65,22 @@ export default {
 
 <template>
   <div class="container">
-    <h1>Home</h1>
-    <table class="table">
+    <!-- Form to add new item -->
+    <form @submit.prevent="addItem">
+      <div class="mb-3">
+        <label for="content" class="form-label">Content</label>
+        <input
+          type="text"
+          id="content"
+          v-model="newContent.content"
+          class="form-control"
+          required
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Add Content</button>
+    </form>
+
+    <table class="table mt-3">
       <thead>
         <tr>
           <th>ID</th>
@@ -47,5 +99,4 @@ export default {
     </table>
   </div>
 </template>
-
 <style scoped></style>
