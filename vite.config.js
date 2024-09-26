8a4ddc48212d09488a2725a5d683edad24756cf0
Panurat <paunavt5@gmail.com>
2024-09-26 13:20:25 +0700

    update docker for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: true, // เพิ่มบรรทัดนี้
     port: 3030,
     proxy: {
       "/api": {
